38ae32282ae54469290a8fe91bd33c3c1840ab05
Evaldo Harris <evaldo.joaoj@hotmail.com>
2026-04-20 09:00:17 -0300

    correção do pdf

--- src/PortfolioPage.jsx
+++ src/PortfolioPage.jsx
@@ -30,6 +30,7 @@ export default function PortfolioPage() {
       image: { type: "jpeg", quality: 0.98 },
       html2canvas: { scale: 2, useCORS: true },
       jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+      pagebreak: { mode: ["avoid-all", "css", "legacy"] },
     };
 
     html2pdf()
